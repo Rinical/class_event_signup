f2496a0b44acf2f00e9d67179ceff3a8fff517e2
Rinical <1847127566@qq.com>
2026-06-11 20:42:49 +0800

    Update event_info.py

--- data/event_info.py
+++ data/event_info.py
@@ -1,3 +1,3 @@
-EVENT_NAME = "班级活动报名与统计网页"
-EVENT_DESCRIPTION = "用 Flask 做一个可以填写表单、提交报名、查看统计结果的动态网页。"
-EVENT_NOTICE = "本项目由 5 人小组通过 GitHub Issue、Fork、Pull Request 和 Review 协作完成。"
+EVENT_NAME = "班级活动名称"
+EVENT_DESCRIPTION = "活动描述说明"
+EVENT_NOTICE = "协作提示信息"
